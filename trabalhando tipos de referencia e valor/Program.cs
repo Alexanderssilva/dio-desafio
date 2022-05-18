a8a61034974b2879ce414982a28ac4511ecc5e88
Alexanderssilva <alexander.s.silva@hotmail.com>
2022-05-17 22:29:44 -0300

    series

---
+++ trabalhando tipos de referencia e valor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace trabalhando_tipos_de_referencia_e_valor
+{
+    public class Program
+    {
+        static int Adicionar20(int x)
+        {
+         return x + 20;
+        }
+        public static void Main()
+        {
+            int a = 2;
+            a = Adicionar20(a);
+            WriteLine(a);
+        }
+    }
+}
