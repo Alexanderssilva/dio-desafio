94a82203ef5f444c4a80e07b4c31ba0926b55408
Alexanderssilva <alexander.s.silva@hotmail.com>
2022-05-23 22:42:01 -0300

    1

---
+++ System.IO/File_and_FileInfo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace File_and_FileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine(Environment.CurrentDirectory +"\\teste.txt");
+            using var sw = File.CreateText(path);
+            sw.WriteLine("Esta é a 1 linha");
+            sw.WriteLine("Esta é a 2 linha");
+            sw.WriteLine("Esta é a 3 linha");
+            sw.WriteLine("Esta é a 4 linha");
+        }
+    }
+}
